e3451480a9b64378b1a5c0900c6db0dad36e2bff
AnisHerdev <88375763+AnisHerdev@users.noreply.github.com>
2025-04-27 15:36:59 +0530

    Fixed few bugs

--- youtube_speed_change.js
+++ youtube_speed_change.js
@@ -1,19 +1,27 @@
 //main function that updates the speed of the video
 var videoSpeed = 1.0; // initial speed
+var speedJump = 0.25; // default speed increment
+
 function setVideoSpeed(speed){
-    const video = document.querySelector('video');
-    video.playbackRate = speed;
+    // const video = document.querySelector('video');
+    // video.playbackRate = speed;
+    const videos = document.querySelectorAll('video');
+    videos.forEach(video => {
+        video.playbackRate = speed;
+    });
     console.log(`Video speed set to ${speed}`);
 }
 
 document.addEventListener('keydown', (event)=>{
     if(event.key === '+'){
-        if (videoSpeed<=10) {
-            videoSpeed += 0.25;
+        videoSpeed += speedJump;
+        if (videoSpeed>10) {
+            videoSpeed = 10; // max speed limit
         }
     } else if(event.key === '-'){
-        if (videoSpeed>=0.25) {
-            videoSpeed -= 0.25;
+        videoSpeed -= speedJump;
+        if (videoSpeed<0) {
+            videoSpeed = 0; // min speed limit
         }
     }
     setVideoSpeed(videoSpeed);
