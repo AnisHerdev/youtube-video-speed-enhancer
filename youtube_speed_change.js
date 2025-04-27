0ab7b2fad4edc59cf328d406eead352e5d278ab6
AnisHerdev <88375763+AnisHerdev@users.noreply.github.com>
2025-04-27 14:42:18 +0530

    Done with the main code that changes the speed

--- youtube_speed_change.js
+++ youtube_speed_change.js
@@ -1,2 +1,16 @@
 //main function that updates the speed of the video
+var videoSpeed = 1.0; // initial speed
+function setVideoSpeed(speed){
+    const video = document.querySelector('video');
+    video.playbackRate = speed;
+    console.log(`Video speed set to ${speed}`);
+}
 
+document.addEventListener('keydown', (event)=>{
+    if(event.key === '+'){
+        videoSpeed += 0.25;
+    } else if(event.key === '-'){
+        videoSpeed -= 0.25;
+    }
+    setVideoSpeed(videoSpeed);
+})
